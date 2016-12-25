2f26e395dc5dcc82370237b72a671b4f76c81eb5
Nghia Tran <nghiattran3@gmail.com>
2016-12-24 21:26:57 -0500

    finish translating lib/ and partial test/

--- test.js
+++ test.js
@@ -1,13 +1,51 @@
 'use strict';
 
-var assert = require('assert');
-var mentality = require('./');
+let assert = require('assert');
+let fs = require('fs');
+let Network = require('./').Network;
 
-describe('test', function(){
+describe('Test fromJson/toJson', function(){
+  function checkNeurons(first, second) {
+    assert(first.activation === second.activation);
+    assert(first.threshold === second.threshold);
+  }
 
-  // You should add more tests here
+  function checkLayers(first, second) {
+    assert(first.neurons.length === second.neurons.length);
+
+    for (var i = 0; i < first.neurons.length; i++) {
+      checkNeurons(first.neurons[i], second.neurons[i])
+    }
+  }
+
+  function checkNetworks(first, second) {
+    checkLayers(first.input, second.layers.input);
+
+    assert(first.hidden.length === second.layers.hidden.length);
+    for (var i = 0; i < first.hidden.length; i++) {
+      checkLayers(first.hidden[i], second.layers.hidden[i]);
+    }
+
+    checkLayers(first.output, second.layers.output);
+  }
+
+  let network;
+  let networkJson;
+  before(function () {
+    networkJson = fs.readFileSync('./test/test_network.json').toString();
+    networkJson = JSON.parse(networkJson);
+    network = Network.fromJson(networkJson);
+  })
   
-  it('fail', function() {
-    assert(false);
+  it('fromJson', function() {
+    network = Network.fromJson(networkJson);
+
+    checkNetworks(network, networkJson);
+  });
+
+  it('toJson', function() {
+    network = Network.fromJson(networkJson);
+    let json = network.toJson();
+    checkNetworks(network, json);
   });
 });

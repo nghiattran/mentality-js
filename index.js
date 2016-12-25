c0e2d8e6c4f93d7c1d98f027f36f1f395e03446c
Nghia Tran <nghiattran3@gmail.com>
2016-12-24 21:56:48 -0500

    fix travis

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
 'use strict';
 
 let Perceptron = require('./lib/network').Perceptron;
+let Network = require('./lib/network').Network;
 let Trainer = require('./lib/trainer').Trainer;
 let Cost = require('./lib/trainer').Cost;
+let Neuron = require('./lib/neuron').Neuron;
+let squash = require('./lib/neuron').squash;
+let Connection = require('./lib/connection').Connection;
 let fs = require('fs');
 
-// module.exports = function(){
-// };
+module.exports = {
+  Perceptron, Trainer, Cost, Neuron, squash, Connection, Network
+};
 
-// let my = new Perceptron(2, [20], 1);
-// my.initialize();
-// let trainer = new Trainer(my);
-
-// trainer.XOR({
-//   log: 1,
-//   // epoch: 2
-// })
-
-let content = fs.readFileSync('./test/test_network.json').toString();
-content = JSON.parse(content);
-console.log(content);
-
-let network = Perceptron.fromJson(content);
+// let content = fs.readFileSync('./sample/test_network.json').toString();
+// content = JSON.parse(content);
+// let network = Perceptron.fromJson(content);
 // console.log(network);
-let trainer = new Trainer(network)
-let settings = {
-    'momentum': 0.99,    
-    'log': 1,
-    epoch: 2,
-    'error': 0.1,
-    'cost': Cost.SE
-}
+// let network = new Perceptron(2, [20], 1);
+// let trainer = new Trainer(network)
+// let settings = {
+//     'momentum': 0.,    
+//     'log': 1,
+//     // epoch: 2,
+//     'error': 0.01,
+//     'cost': Cost.SE
+// }
 
-trainer.XOR(settings)
-console.log(network.toJson());
+// let res = trainer.AND(settings)
+// console.log(res);
+// console.log(network.toJson());

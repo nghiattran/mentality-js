f882121bee64e4da405fdf648b59dce0c0cd07ae
Nghia Tran <nghiattran3@gmail.com>
2016-12-27 16:35:43 -0500

    partially done #2, test and optimization are still needed

--- test.js
+++ test.js
@@ -7,53 +7,160 @@ let Perceptron = require('./').Perceptron;
 let Trainer = require('./').Trainer;
 let Layer = require('./').Layer;
 let ValueError = require('./').ValueError;
+let Neuron = require('./').Neuron;
 
+describe('Test fromJson/toJson', function() {
+  describe('Network', function() {
+    let network;
+    let networkJson;
+    before(function () {
+      networkJson = fs.readFileSync('./sample/test_network.json').toString();
+      networkJson = JSON.parse(networkJson);
+      network = Network.fromJson(networkJson);
+    })
+    
+    it('fromJson', function() {
+      network = Network.fromJson(networkJson);
+      assert(network.compareTo(networkJson.layers));
+    });
 
-describe('Test fromJson/toJson', function(){
-  function checkNeurons(first, second) {
-    assert(first.activation === second.activation);
-    assert(first.threshold === second.threshold);
-  }
-
-  function checkLayers(first, second) {
-    assert(first.neurons.length === second.neurons.length);
-
-    for (var i = 0; i < first.neurons.length; i++) {
-      checkNeurons(first.neurons[i], second.neurons[i])
-    }
-  }
-
-  function checkNetworks(first, second) {
-    checkLayers(first.input, second.layers.input);
-
-    assert(first.hidden.length === second.layers.hidden.length);
-    for (var i = 0; i < first.hidden.length; i++) {
-      checkLayers(first.hidden[i], second.layers.hidden[i]);
-    }
-
-    checkLayers(first.output, second.layers.output);
-  }
-
-  let network;
-  let networkJson;
-  before(function () {
-    networkJson = fs.readFileSync('./sample/test_network.json').toString();
-    networkJson = JSON.parse(networkJson);
-    network = Network.fromJson(networkJson);
+    it('toJson', function() {
+      network = Network.fromJson(networkJson);
+      let json = network.toJson();
+      assert(network.compareTo(json.layers));
+    });
   })
-  
-  it('fromJson', function() {
-    network = Network.fromJson(networkJson);
-
-    checkNetworks(network, networkJson);
-  });
-
-  it('toJson', function() {
-    network = Network.fromJson(networkJson);
-    let json = network.toJson();
-    checkNetworks(network, json);
-  });
 });
+
+describe('Test object comparation', function() {
+  describe('Perceptron', function() {
+    it('Compare 2 different Perceptrons', function() {
+      let network1 = new Perceptron(2, [1, 1], 1);
+      let network2 = new Perceptron(1, [], 1);
+
+      assert(!network1.compareTo(network2));
+    });
+
+    it('Compare 2 different but similar Perceptrons', function() {
+      let network1 = new Perceptron(1, [1, 1, 1, 1], 1);
+      let network2 = new Perceptron(1, [1, 1, 1, 1], 1);
+
+      assert(!network1.compareTo(network2));
+    });
+
+    it('Compare 2 identical Perceptrons', function() {
+      let network1 = new Perceptron(2, [1, 1, 1, 1], 1);
+      let network2 = Perceptron.fromJson(network1.toJson())
+
+      assert(network1.compareTo(network2));
+    });
+  })
+
+  describe('Network', function() {
+    it('Compare 2 different Networks', function() {
+      let layer11 = new Layer(5);
+      let layer12 = new Layer(5);
+      layer11.project(layer12);
+      let network1 = new Network({
+        input: layer11,
+        output: layer12
+      });
+
+      let layer21 = new Layer(5);
+      let layer22 = new Layer(5);
+      let layer23 = new Layer(5);
+      let layer24 = new Layer(5);
+      layer21.project(layer22);
+      layer22.project(layer23);
+      layer23.project(layer24);
+      let network2 = new Network({
+        input: layer21,
+        hidden: [layer22, layer23],
+        output: layer24
+      });
+
+      assert(!network1.compareTo(network2));
+    });
+
+    it('Compare 2 different but similar Networks', function() {
+      let layer11 = new Layer(5);
+      let layer12 = new Layer(5);
+      layer11.project(layer12);
+      let network1 = new Network({
+        input: layer11,
+        output: layer12
+      });
+
+      let layer21 = new Layer(5);
+      let layer22 = new Layer(5);
+      layer21.project(layer22);
+      let network2 = new Network({
+        input: layer21,
+        output: layer22
+      });
+
+      assert(!network1.compareTo(network2));
+    });
+
+    it('Compare 2 identical Networks', function() {
+      let layer11 = new Layer(5);
+      let layer12 = new Layer(5);
+      layer11.project(layer12);
+
+      let network1 = new Network({
+        input: layer11,
+        output: layer12
+      });
+
+      let network2 = new Network({
+        input: layer11,
+        output: layer12
+      });
+
+      assert(network1.compareTo(network2));
+    });
+  })
+
+  describe('Layer', function() {
+    it('Compare 2 different Layers', function() {
+      let layer1 = new Layer(10);
+      let layer2 = new Layer(6);
+
+      assert(!layer1.compareTo(layer2));
+    });
+
+    it('Compare 2 different but similar Layers', function() {
+      let layer1 = new Layer(10);
+      let layer2 = new Layer(10);
+
+      assert(!layer1.compareTo(layer2));
+    });
+
+    it('Compare 2 identical Layers', function() {
+      let layer1 = new Layer(10);
+      let layer2 = Layer.fromJson(layer1.toJson());
+
+      assert(layer1.compareTo(layer2));
+    });
+  })
+
+  describe('Neuron', function() {
+    it('Compare 2 different Layers', function() {
+      let neuron1 = new Neuron();
+      let neuron2 = new Neuron();
+
+      assert(!neuron1.compareTo(neuron2));
+    });
+
+    it('Compare 2 identical Layers', function() {
+      let neuron1 = new Neuron(10);
+      let neuron2 = Neuron.fromJson(neuron1.toJson());
+      // console.log(neuron2);
+      // console.log(neuron1);
+      assert(neuron1.compareTo(neuron2));
+    });
+  })
+})
 
 describe('Test Perceptron', function() {
   function sum(arr) {
@@ -191,4 +298,8 @@ describe('Test error handler', function() {
       assert.throws(project, ValueError);
     });
   })
+})
+
+describe('Test master network', function() {
+
 })

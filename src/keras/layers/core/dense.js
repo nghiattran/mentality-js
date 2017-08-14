7dce535bf5b7b4c37fbb7bc4f0fc46f3cfe7aab8
nghiattran <nghiattran3@gmail.com>
2017-08-14 13:43:30 -0400

    update node-parser

--- src/keras/layers/core/dense.js
+++ src/keras/layers/core/dense.js
@@ -4,17 +4,17 @@ const Layer = require('../layer');
 const Weights = require('../../weights');
 
 /* 
-@form Dense
+@node Dense
 @description Dense implements the operation: output = activation(dot(input, kernel) + bias) where activation is the element-wise activation function passed as the activation argument, kernel is a weights matrix created by the layer, and bias is a bias vector created by the layer (only applicable if use_bias is True).
 
-@input    units
+@property    units
 @type     number
 @required true
 @step     0
 @min      1
 @description Positive integer, dimensionality of the output space.
 
-@input activation
+@property activation
 @type     text
 
 @description     Activation function to use (see activations). If you don't specify anything, no activation is applied (ie. "linear" activation: a(x) = x)

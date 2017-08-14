29dd8c07aa3796ae2546dcdc76f124d08148ff5f
nghiattran <nghiattran3@gmail.com>
2017-08-14 12:59:51 -0400

    add missing include folder

--- src/keras/layers/core/dense.js
+++ src/keras/layers/core/dense.js
@@ -3,6 +3,23 @@ const lodashDefaults = require('lodash').defaults;
 const Layer = require('../layer');
 const Weights = require('../../weights');
 
+/* 
+@form Dense
+@description Dense implements the operation: output = activation(dot(input, kernel) + bias) where activation is the element-wise activation function passed as the activation argument, kernel is a weights matrix created by the layer, and bias is a bias vector created by the layer (only applicable if use_bias is True).
+
+@input    units
+@type     number
+@required true
+@step     0
+@min      1
+@description Positive integer, dimensionality of the output space.
+
+@input activation
+@type     text
+
+@description     Activation function to use (see activations). If you don't specify anything, no activation is applied (ie. "linear" activation: a(x) = x)
+ */
+
 /**
  * Regular fully-connected layer.
  * @extends mentality.keras.layers.Layer
